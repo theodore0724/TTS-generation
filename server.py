6efd6b14ee99732fefffe21f62836b06bd32a513
theodore0724 <theodorejin0724@gmail.com>
2023-08-26 13:34:54 +0800

    add favorites and delete options

--- server.py
+++ server.py
@@ -131,6 +131,18 @@ def load_models(
 def open_outputs_folder():
     os.startfile("outputs")
 
+def open_favorites_folder():
+    os.startfile("favorites")
+
+def delete_generation_cb(refresh):
+    def delete_generation(json):
+        os.remove(json["filename"])
+        os.remove(json["filename_png"])
+        os.remove(json["filename_json"])
+
+        return refresh()
+
+    return delete_generation
 
 # generate audio from text
 def generate(prompt, useHistory, language=None, speaker_id=0, text_temp=0.7, waveform_temp=0.7):
@@ -161,6 +173,7 @@ def generate(prompt, useHistory, language=None, speaker_id=0, text_temp=0.7, wav
     filename_png = f"{base_filename}.png"
     plt.savefig(filename_png)
 
+    filename_json = f"{base_filename}.json"
     # Generate metadata for the audio file
     metadata = {
         "prompt": prompt,
@@ -172,9 +185,10 @@ def generate(prompt, useHistory, language=None, speaker_id=0, text_temp=0.7, wav
         "date": date,
         "filename": filename,
         "filename_png": filename_png,
+        "filename_json": filename_json,
     }
     # Write metadata to txt file
-    with open(f"{base_filename}.json", "w") as outfile:
+    with open(filename_json, "w") as outfile:
         json.dump(metadata, outfile, indent=2)
 
     return [filename, filename_png]
@@ -317,6 +331,23 @@ with gr.Blocks() as block:
         history_image = gr.Image()
         history_json = gr.JSON()
 
+        def save_to_favorites(json_text):
+            shutil.copy(json_text["filename"], "favorites/")
+            shutil.copy(json_text["filename_png"], "favorites/")
+            # write new json file with new filename
+            json_text["filename"] = json_text["filename"].replace("outputs/", "favorites/")
+            json_text["filename_png"] = json_text["filename_png"].replace("outputs/", "favorites/")
+            json_text["filename_json"] = json_text["filename_json"].replace("outputs/", "favorites/")
+            with open(json_text["filename_json"].replace("outputs/", "favorites/"), "w") as f:
+                json.dump(json_text, f, indent=2)
+            return gr.Button.update(value="Saved")
+
+        with gr.Row():
+            delete_from_history = gr.Button(value="Delete", variant="stop", visible=False)
+            save_to_favorites_history = gr.Button(value="Save to favorites", variant="primary", visible=False)
+
+        save_to_favorites_history.click(fn=save_to_favorites, inputs=history_json, outputs=save_to_favorites_history)
+
         def select_audio(selection):
             filename = wav_file_list[selection]
             with open(filename.replace(".wav", ".json")) as f:
@@ -325,13 +356,71 @@ with gr.Blocks() as block:
                 gr.Audio.update(value=filename, label=filename),
                 gr.Image.update(value=filename.replace(".wav", ".png")),
                 gr.JSON.update(value=json_text),
+                gr.Button.update(visible=True),
+                gr.Button.update(visible=True, value="Save to favorites"),
             ]
 
         history_list.select(fn=select_audio, inputs=[history_list], outputs=[
-                            history_audio, history_image, history_json], preprocess=False)
+                            history_audio, history_image, history_json, delete_from_history, save_to_favorites_history], preprocess=False)
 
         def update_history_tab():
             return gr.Dataset.update(samples=get_wav_files())
+        delete_from_history.click(fn=delete_generation_cb(update_history_tab), inputs=history_json, outputs=[history_list])
+        history_tab.select(fn=update_history_tab, outputs=[history_list])
+
+    with gr.Tab("Favorites") as favorites_tab:
+
+        with gr.Row():
+            button_output = gr.Button(value="Open favorites folder")
+        button_output.click(open_favorites_folder)
+
+        wav_file_list_favorites = []
+
+        def get_wav_files_favorites():
+            wav_files = glob.glob("favorites/*.wav")
+            global wav_file_list_favorites
+            wav_file_list_favorites = wav_files
+            return [[file] for file in wav_files]
+
+        _audio_component = gr.Audio(visible=False)
+        favorites_list = gr.Dataset(components=[_audio_component],
+                                  samples=get_wav_files_favorites(), label="Favorites", samples_per_page=20)
+        favorites_audio = gr.Audio(
+            visible=True, type="filepath", label="Favorites")
+        favorites_image = gr.Image()
+        favorites_json = gr.JSON()
+
+        with gr.Row():
+            delete_from_favorites = gr.Button(value="Delete", variant="stop", visible=False)
+
+        def select_audio(selection):
+            filename = wav_file_list_favorites[selection]
+            with open(filename.replace(".wav", ".json")) as f:
+                json_text = json.load(f)
+            return [
+                gr.Audio.update(value=filename, label=filename),
+                gr.Image.update(value=filename.replace(".wav", ".png")),
+                gr.JSON.update(value=json_text),
+                gr.Button.update(visible=True),
+            ]
+
+        def clear_audio():
+            return [
+                gr.Audio.update(value=None, label=""),
+                gr.Image.update(value=None),
+                gr.JSON.update(value=None),
+                gr.Button.update(visible=False),
+            ]
+
+        favorites_list.select(fn=select_audio, inputs=[favorites_list], outputs=[
+                            favorites_audio, favorites_image, favorites_json, delete_from_favorites], preprocess=False)
+
+        def update_favorites_tab():
+            return gr.Dataset.update(samples=get_wav_files_favorites())
+
+        delete_from_favorites.click(fn=clear_audio, outputs=[favorites_audio, favorites_image, favorites_json, delete_from_favorites])
+        delete_from_favorites.click(fn=delete_generation_cb(update_favorites_tab), inputs=favorites_json, outputs=[favorites_list])
+        favorites_tab.select(fn=update_favorites_tab, outputs=[favorites_list])
 
     with gr.Tab("Settings") as settings_tab:
         with gr.Row():
@@ -475,7 +564,5 @@ with gr.Blocks() as block:
                     codec_use_gpu
                 ], outputs=[load_button], show_progress=True)
 
-    history_tab.select(fn=update_history_tab, inputs=[],
-                       outputs=[history_list])
 
     block.launch(server_name='0.0.0.0')
