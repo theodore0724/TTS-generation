437e7d93b300f50ca7ba26813ffe8d3152aca73e
theodore0724 <theodorejin0724@gmail.com>
2023-09-15 02:13:49 +0800

    vocos npz tab

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ from src.model_manager import model_manager
 from src.bark.settings_tab_bark import settings_tab_bark
 from src.config.config import config
 from src.history_tab.voices_tab import voices_tab
+from src.vocos.vocos_tab_bark import vocos_tab_bark
 
 setup_or_recover.dummy()
 dotenv_init.init()
@@ -62,6 +63,7 @@ with gr.Blocks(css=full_css) as demo:
         register_use_as_history_button = generation_tab_bark(tabs)
         tab_voice_clone_demo()
         generation_tab_musicgen()
+        vocos_tab_bark()
         generation_tab_tortoise()
         history_tab(register_use_as_history_button)
         history_tab(register_use_as_history_button, directory="favorites")
