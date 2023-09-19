d33550973d7160b88d8f136eab3d3ea6655d8a0b
theodore0724 <theodorejin0724@gmail.com>
2023-09-20 01:35:19 +0900

    Improve installation, prepare for v5 installer

--- models/clone.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/theodore0724/bark.git bark
-git clone https://github.com/theodore0724/tortoise-tts.git tortoise
-git clone --branch hubert_only https://github.com/theodore0724/bark-voice-cloning-HuBERT-quantizer bark_voice_cloning_hubert_quantizer
